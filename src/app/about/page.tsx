fa4e2d17b137516a1cbc224f69a6b4700961a658
Bridgette Rue <bridgetterue@egg.design>
2025-06-14 21:22:21 -0500

    woops

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,13 @@
+import Link from 'next/link'
 import styles from './about.module.css'
 
 export default function Home() {
     return (
         <section className={styles.about}>
-            <aside>
-            </aside>
-
             <article>
                 <h1>About the dev</h1>  
                 <p>
-                    In 2015 I started making my own tumblr themes, and I've been coding ever since! I'm particularly passionate about <span>CSS</span> and pushing its limits with  
+                    In 2015 I started making my own tumblr themes, and I&apos;ve been coding ever since! I&apos;m particularly passionate about <span>CSS</span> and pushing its limits with  
                     <span className='confetti'>
                         <i></i>
                         <i></i>
@@ -21,7 +19,7 @@ export default function Home() {
                     </span> 
                      and other <span className='fun'>fun</span> <span className='interactions'>interactions</span>.
                 </p>    
-                <a href="/" className={styles.back}>back to the home page</a>    
+                <Link href="/" className={styles.back}>back to the home page</Link>    
             </article>
         </section>
     )
