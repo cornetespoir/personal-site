0c84f60ac72b5fe814d015a60a4df96f40d7e4d7
Bridgette Rue <bridgetterue@egg.design>
2025-06-15 19:28:32 -0500

    updated project

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -1,4 +1,11 @@
 export const projects = [
+       {
+        image: 'https://64.media.tumblr.com/96afdb6c0d731bb2d6031582dea7da97/4b8a757f84bd9c0e-0d/s1280x1920/2a3ae04dfd4438825510c2a6292b1a486301ff60.pnj',
+        altText: 'Screenshot of a search tool',
+        title: 'Findtags',
+        description: 'An unfiltered (by default) tag search for tumblr posts by getting all posts directly from the API',
+        url: 'https://findtags.egg.design/'
+    }, 
     {
         title: 'Maestro',
         image: 'https://64.media.tumblr.com/b8324530d946f5606057179cff71ca12/909224c0fa958c48-ad/s1280x1920/d7ce354284b7bf72bf702ae23b3e5e91044bbc5d.pnj',
@@ -20,11 +27,5 @@ export const projects = [
         description: 'A basic tumblr theme template that supports new post types',
         url: 'https://github.com/cornetespoir/npf-theme-base'
     },
-    {
-        image: 'https://64.media.tumblr.com/31786906ed286c0edbd4183a82ef4d62/4b8a757f84bd9c0e-e5/s1280x1920/a0f9a00a830c0c165342d8b5a0788424ca9f8c84.pnj',
-        altText: 'Screenshot of a search tool',
-        title: 'Findtags',
-        description: 'An unfiltered (by default) tag search for tumblr posts by getting all posts directly from the API',
-        url: 'https://github.com/cornetespoir/findtags-react'
-    }
+
 ]
